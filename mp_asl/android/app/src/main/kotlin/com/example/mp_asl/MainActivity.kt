e1b2f625ff53eb34d1053cb3ae6546d9044688ae
something411 <54055822+something411@users.noreply.github.com>
2022-05-05 19:28:10 +0530

    flutter commit

---
+++ mp_asl/android/app/src/main/kotlin/com/example/mp_asl/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mp_asl
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mp_asl/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ASL Machine Learning',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage();
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('me when the'),
+      ),
+      body: Center(child: Text("Here comes the Machien Learning part!")),
+    );
+  }
+}
